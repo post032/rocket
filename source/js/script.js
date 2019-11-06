89f0bbe46dc296c81be67fa5adb89163aabcbff1
Elena Ordina <post032@yandex.ru>
2019-11-07 02:17:41 +0300

    настройка ночного режима + инлайн svg

--- source/js/script.js
+++ source/js/script.js
@@ -4,8 +4,11 @@
   var logo = document.querySelector('.menu__logo');
   var show = document.querySelector('.menu__nav');
   var toggle = document.querySelector('.menu__range');
-  var body = document.querySelector('body')
-  var revers = document.querySelector('.revers');
+  var day = document.querySelector('.menu__day');
+  var night = document.querySelector('.menu__night');
+  var about = document.querySelector('.about-us');
+  var adv = document.querySelector('.advantages');
+  var body = document.querySelector('body');
 
   open.addEventListener('click', function (e) {
     e.preventDefault();
@@ -13,8 +16,6 @@
       show.classList.remove('menu__nav--close');
       open.classList.remove('menu__open');
       open.classList.add('menu__close');
-      revers.classList.add('no-revers__logo');
-      revers.classList.remove('revers__logo');
       logo.classList.add('menu__logo--active');
       menu.classList.add('menu--active');
     }
@@ -22,9 +23,35 @@
       show.classList.add('menu__nav--close');
       open.classList.add('menu__open');
       open.classList.remove('menu__close');
-      revers.classList.remove('no-revers__logo');
-      revers.classList.add('revers__logo');
       logo.classList.remove('menu__logo--active');
       menu.classList.remove('menu--active');
     }
   });
+
+day.addEventListener('click', function (e) {
+  e.preventDefault();
+  if(day.classList.contains('menu__active')) {
+  }
+  else {
+    day.classList.add('menu__active');
+    night.classList.remove('menu__active');
+    about.classList.remove('night');
+    menu.classList.remove('night');
+    adv.classList.remove('night');
+    body.classList.remove('night');
+  }
+});
+
+night.addEventListener('click', function (e) {
+  e.preventDefault();
+  if(night.classList.contains('menu__active')) {
+  }
+  else {
+    day.classList.remove('menu__active');
+    night.classList.add('menu__active');
+    about.classList.add('night');
+    menu.classList.add('night');
+    adv.classList.add('night');
+    body.classList.add('night');
+  }
+});
